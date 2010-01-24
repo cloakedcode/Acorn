080012eea61abdc7f7b79dd8dcfed0293fdf6759
Alan Smith <skrat19@gmail.com>
2010-01-23 22:36:49 -0500

    Edited the comments in the demo controller.

--- app/controllers/sample_controller.php
+++ app/controllers/sample_controller.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This doesn't work, yet, it's simply a demo of how Acorn would be used.
+ * This works beautifully, check it out! (http://localhost/acorn/)
  *
  */
 
@@ -11,7 +11,6 @@
 	 {
 		 $this->posts = Post::query(Post::$find_by_id, 3079);
 
-		 // This line is still needed, renderView will (eventually) automatically be called if $this->should_render is true (it is be default).
 		 $this->renderView('index');
 	 }
  }
